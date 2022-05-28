0b34e09718da1a549b315c473b000328ee44627c
Skyyyr <emuswgluvr@gmail.com>
2022-05-28 13:54:35 -0700

    Py Pad Array

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,28 @@
-#REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    """Take 3 params and determine if you can padd the array or not; then return it
+
+    Args:
+        array (array): An array of elements
+        min_size (int): Padding size if it's greater than the length of the array
+        value (any, optional): If padding occures padd with this value. Defaults to None.
+
+    Returns:
+        array: array that might have been padded with a value
+    """
+
+    # If the length of the array is greater than or equal to the min_size variable then we return the array
+    if len(array) >= min_size:
+        return array
+
+    # Iterate to append to array with the value
+    for i in range(len(array), min_size):
+        array.append(value)
+
+    # Return the padded array
+    return array
+
+print(pad([1,2,3], 3));
+print(pad([1,2,3], 4, 4));
+print(pad([1,2,3], 5));
+print(pad(["a", "b", "c"], 10, "?"));
+print(pad(["a", "b", "c"], 1, "?"));
